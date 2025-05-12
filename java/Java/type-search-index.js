04e670379a1c1c97a3db425b6bca93dfa0dcfb58
Mayank <mayankwanjari45@gmail.com>
2025-05-12 23:39:10 +0530

    index javascript

---
+++ java/Java/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"GuesserGameApp"},{"p":"<Unnamed>","l":"Gusser"},{"p":"<Unnamed>","l":"Player"},{"p":"<Unnamed>","l":"Umpire"}];updateSearchResults();
